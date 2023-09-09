ad559f8dc6dd74b161371ac6ae1f73055903c0cc
example <e@example.com>
2023-09-09 17:55:46 +0300

    portfolio

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "./styles/main.css";
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Project from './pages/Project';
+import Contacts from './pages/Contacts';
+import ScrollToTop from './utils/ScrollToTop';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <ScrollToTop/>
+        <Navbar/>
+       
+<Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/project/:id" element={<Project/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+          <Route path="Contacts" element={<Contacts/>}></Route>
+          
+          
+        </Routes>
+
+
+      <Footer/>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
